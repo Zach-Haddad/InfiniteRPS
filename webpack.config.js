44253a6ca2e8f3a403b48b2d3e1586067672cde4
Zach-Haddad <Zhaddad7@gmail.com>
2017-01-25 11:48:11 -0800

    refactored input

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,17 @@ module.exports = {
   output: {
   	filename: "./lib/bundle.js"
   },
+  module: {
+    loaders: [
+      {
+        test: [/\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
   devtool: 'source-map',
 };
